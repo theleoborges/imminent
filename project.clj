26537c2646a738af453c82fad1d2c08114a28d34
Leonardo Borges <leonardoborges.rj@gmail.com>
2015-02-08 14:36:07 +1100

    Update generated docs

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.leonardoborges/imminent "0.1.0"
-  :description "FIXME: write description"
+  :description "A composable Futures library for Clojure"
   :url "https://github.com/leonardoborges/imminent"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
