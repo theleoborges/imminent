827f3978b8753a9bd6bb90b817ae56829d13d44c
Leonardo Borges <leonardoborges.rj@gmail.com>
2015-03-19 12:25:28 -0300

    Initial docs commit

--- CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-## 0.1.0 (not released)
-
-- Initial release.
--- src/imminent/util/functor.clj
+++
@@ -1,6 +0,0 @@
-(ns imminent.util.functor)
-
-(defprotocol BiFunctor
-  "Functor of two arguments"
-  (bimap [fv f g]
-    "Map over both arguments at the same time"))
