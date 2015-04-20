ae0cc34097a1be1c0d6314f98eb2dded6d8de4a2
Leonardo Borges <leonardoborges.rj@gmail.com>
2015-04-20 15:32:05 +1000

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.leonardoborges/imminent "0.1.1"
+(defproject com.leonardoborges/imminent "0.1.2"
   :description "A composable Futures library for Clojure"
   :url "https://github.com/leonardoborges/imminent"
   :license {:name "Eclipse Public License"
