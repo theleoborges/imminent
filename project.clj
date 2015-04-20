d5ec79534647be97560976d1e8f9611bbe6fdba7
Leonardo Borges <leonardoborges.rj@gmail.com>
2015-04-20 14:42:33 +1000

    Update clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/leonardoborges/imminent"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-beta1"]
                  [org.clojure/test.check "0.5.8"]
                  [uncomplicate/fluokitten "0.3.0"]
                  [org.clojure/core.match "0.2.1"]]
