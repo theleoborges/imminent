f0f04cfd947bbdfed755d80f6713362b283f9050
Leonardo Borges <leonardoborges.rj@gmail.com>
2019-08-27 20:22:26 +1200

    Upgrade Clojure to 1.10 and midje to 1.9

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/leonardoborges/imminent"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/test.check "0.5.8"]
                  [uncomplicate/fluokitten "0.3.0"]
                  [org.clojure/core.match "0.2.1"]]
   :plugins [[codox "0.8.10"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
+  :profiles {:dev {:dependencies [[midje "1.9.9"]]}})
